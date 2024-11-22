7d7ff091853cedefbbf21039360817854d773fdb
Gauravjangid292004 <gauravjangid292004@gmail.com>
2024-11-22 11:06:12 +0530

    Add files via upload

---
+++ bookstore/bookstore/src/main/java/SpringApplication.java
@@ -0,0 +1,10 @@
+package com.crni99.bookstore;
+
+public class SpringApplication {
+
+	public static void run(Class<BookStoreApplication> class1, String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print(false);
+	}
+
+}
